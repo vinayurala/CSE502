5ca56c0fe2b840ba0bb35d74227bc26ab2b1cb7f
Vinay <vinay@ubuntuvm.(none)>
2013-02-03 01:16:25 -0500

    HW1 Part1 implemented as a kern module; needs a lot of modifications

--- hw1.c
+++ hw1.c
@@ -7,19 +7,82 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/preempt.h>
+#include <linux/sched.h>
+#include <linux/hardirq.h>
 
-static int __init hello_init(void)
+static int __init timer_init(void)
 {
-  
-  printk(KERN_INFO "Hello, world!\n");
+  unsigned long timer_high_start, timer_high_end, timer_low_start, timer_low_end;
+  unsigned long tot_start, tot_end;//, flags;
+  unsigned long cfq_low_end, cfq_low_start, cfq_high_end, cfq_high_start;
+  unsigned long cfq_tot_start, cfq_tot_end;
+  double clock_freq;
+  volatile int x = 0;
+  int y;
+
+  preempt_disable();
+  // raw_local_irq_save(flags);
+
+  __asm__ __volatile("cli \n\t"
+		     "cpuid \n\t"
+		     "rdtsc \n\t"
+		     "mov %%rdx, %0\n\t"
+		     "mov %%rax, %1\n\t"
+		     : "=r"(cfq_high_start), "=r"(cfq_low_start)
+		     :: "%rax", "%rbx", "%rcx", "%rdx");
+  __asm__ __volatile("cpuid \n\t"
+		     "rdtsc \n\t"
+		     "mov %%rdx, %0\n\t"
+		     "mov %%rax, %1\n\t"
+		     "sti \n\t"
+		     : "=r"(cfq_high_end), "=r"(cfq_low_end)
+		     :: "%rax", "%rbx", "%rcx", "%rdx");
+
+  cfq_tot_start = (cfq_high_start << 32) | cfq_low_start;
+  cfq_tot_end = (cfq_high_end << 32) | cfq_low_end;
+
+  clock_freq = cfq_tot_end / cfq_tot_start;
+
+  __asm__ __volatile("cli \n\t"
+		     "cpuid \n\t"
+		     "rdtsc \n\t"
+		     "mov %%rdx, %0\n\t"
+		     "mov %%rax, %1\n\t"
+		     : "=r"(timer_high_start), "=r"(timer_low_start)
+		     :: "%rax", "%rbx", "%rcx", "%rdx");
+
+  for(y = 0; y < 1234567890; y++)
+    ++x;
+
+  __asm__ __volatile("cpuid \n\t"
+		     "rdtsc \n\t"
+		     "mov %%rdx, %0\n\t"
+		     "mov %%rax, %1\n\t"
+		     "sti \n\t"
+		     : "=r"(timer_high_end), "=r"(timer_low_end)
+		     :: "%rax", "%rbx", "%rcx", "%rdx");
+
+  // raw_local_irq_restore(flags);
+  preempt_enable();
+
+  tot_start = (timer_high_start << 32) | timer_low_start;
+  tot_end = (timer_high_end << 32) | timer_low_end;
+
+  printk (KERN_ALERT "Starting timer module");
+  printk (KERN_ALERT "Clock frequency = %f", clock_freq);
+  printk (KERN_ALERT "Total time spent on for loop: %11lu", (tot_end - tot_start));
 
   return 0;
 }
 
-static void __exit hello_exit(void)
+static void __exit timer_exit(void)
 {
   printk(KERN_INFO "Exiting and cleaning up module\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(timer_init);
+module_exit(timer_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Kernel module to measure time spent on a loop");
